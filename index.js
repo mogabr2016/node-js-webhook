e12a5e9a860341d83541b055e31086253ad591a3
mogabr2016 <mogabr2016@gmail.com>
2024-08-15 16:37:44 +0300

    Update index.js
    "messages"

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.post("/webhook",(req,res)=>{
 
     if (body_param.object) {
         if (body_param.entry &&
-            body_param.entry[0].changes[0].value.message&&
-            body_param.entry[0].changes[0].value.message[0]) {
+            body_param.entry[0].changes[0].value.messages&&
+            body_param.entry[0].changes[0].value.messages[0]) {
 
            let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
            let from = body_param.entry[0].changes[0].value.messages[0].from;
